3a95489357ccab43fc1a23cb5934da1401b774c1
DOZE / UILib <afterlife.records@outlook.com>
2026-03-06 12:30:46 +0000

    Update Example.lua

--- Notifications/Ritual/Example.lua
+++ Notifications/Ritual/Example.lua
@@ -1,4 +1,4 @@
-local Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/tacolift/notification-library/main/library.lua"))()
+local Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/DozeIsOkLol/UILibarys/refs/heads/main/Notifications/Ritual/Source.lua"))()
 Notifications.Create("ritualhax or something", "hi welcome to something or idk", 1, Color3.fromRGB(255,255,255))
 -- First argument is the title, second one's the description, third one's the time and the last one's the title color.
 task.wait(1)
