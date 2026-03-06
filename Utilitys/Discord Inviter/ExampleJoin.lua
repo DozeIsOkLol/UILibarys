e8fcf9fb9e5d7e47c9a7af074b1b0dc7221ca39e
DOZE / UILib <afterlife.records@outlook.com>
2026-03-06 13:34:01 +0000

    Create ExampleJoin.lua

---
+++ Utilitys/Discord Inviter/ExampleJoin.lua
@@ -0,0 +1,5 @@
+---====== Load Discord inviter ======---
+local discordInviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/DozeIsOkLol/UILibarys/refs/heads/main/Utilitys/Discord%20Inviter/Source.lua"))()
+
+---====== Join Discord server ======---
+discordInviter.Join("https://discord.com/invite/roblox")
