6724124f4e88a35d7f9e4eff96100bf03485b65e
DOZE / UILib <afterlife.records@outlook.com>
2026-03-06 13:38:13 +0000

    Create Example.lua

---
+++ Utilitys/ESP/3/Example.lua
@@ -0,0 +1,26 @@
+-- Loader
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/DozeIsOkLol/UILibarys/refs/heads/main/Utilitys/ESP/3/Source.lua"))()
+
+-- Services
+local Players = game:GetService("Players")
+
+-- Example
+for _, plr in Players:GetPlayers() do
+    if plr == Players.LocalPlayer or not plr.Character then
+        continue
+    end
+
+    local character = plr.Character
+    local humanoid = character:WaitForChild("Humanoid")
+    local rootPart = character:WaitForChild("HumanoidRootPart")
+
+    module:Add(rootPart, {
+        Name = plr.Name,
+        Color = Color3.fromHSV(math.random(), 1, 1),
+        HighlightFocus = character,
+    }, {
+        Health = function() -- This callback is used to display health, maxHealth
+            return humanoid.Health, humanoid.MaxHealth
+        end
+    })
+end
